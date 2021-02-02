7abefaa506c81adc430f52a7285d0173997dc6a1
OjanayR <69225985+OjanayR@users.noreply.github.com>
2021-02-01 23:11:38 -0500

    works on javascript file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,15 @@
-// coded by @lasjorg
-// eslint-disable-next-line no-unused-vars
-const projectName = 'portfolio';
+/*==== MENU SHOW ====*/
+
+/*==== REMOVE MENU MOBILE ====*/
+
+/*==== SCROLL SECTIONS ACTIVE LINK ====*/
+
+/*==== SCROLL  REVEAL ANIMATION ====*/
+
+/*==== SCROLL HOME ====*/
+
+/*==== SCROLL ABOUT ====*/
+
+/*==== SCROLL SKILLS ====*/
+
+/*====SCROLL PORTFOLIO ====*/
