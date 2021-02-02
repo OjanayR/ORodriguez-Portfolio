118063a955a156f72327086641caa5fcce80a29d
OjanayR <69225985+OjanayR@users.noreply.github.com>
2021-02-01 23:54:18 -0500

    nav bar wip

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,7 +19,27 @@ function linkAction(){
     navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
 /*==== SCROLL SECTIONS ACTIVE LINK ====*/
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectonTop && scrollY <= sectionTop + sectionHeight){
+           document.querySelectorAll('.nav_menu a[href*=' + sectionId + ']').classList.add('active') 
+        }else{
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
 
 /*==== SCROLL  REVEAL ANIMATION ====*/
 
